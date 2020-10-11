dbcf6f6744ff9a60326108d5f474a901ff12eac9
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-11 16:00:59 +0530

    My First Commit For C Programming

---
+++ programiz/flowcontrol/BreakAndContinue.c
@@ -0,0 +1,12 @@
+// Program to calculate the sum of numbers (10 numbers max)
+// If the user enters a negative number, the loop terminates
+#include <stdio.h>
+int main()
+{
+ int i;
+ for(i=1;i<11;++i)
+ {
+     printf("%d\n",i);
+ }
+ return 0;
+} 
