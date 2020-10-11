1afb01814ae2775dc42868b19a774f6a94b494c6
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-11 23:04:28 +0530

    C Programming Introduction

---
+++ programiz/.vscode/Introduction/ASCIIValue.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    char a;
+    printf("Enter a character: ");
+    scanf("%c", &a);     
+    printf("ASCII value is %d.",a);  
+    return 0;
+}
---
+++ programiz/.vscode/Introduction/DataTypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a;
+char b;
+float c;
+double d;
+
+    printf("The size of int is    :%d bytes\n",sizeof(a));
+    printf("The size of char is   :%d bytes\n",sizeof(b));
+    printf("The size of float is  :%d bytes\n",sizeof(c));
+    printf("The size of double is :%d bytes",sizeof(d));
+    return 0;
+}
