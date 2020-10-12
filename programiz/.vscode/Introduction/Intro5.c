9ce719cddee9cead95e25f40a9d2e15c8bd06855
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-12 14:17:14 +0530

    Introduction-problems

---
+++ programiz/.vscode/Introduction/Intro5.c
@@ -0,0 +1,10 @@
+//C Program to Find ASCII Value of a Character
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf("Enter a alphabet: ");
+    scanf("%c",&a);
+    printf("The ASCII value is: %d",a);
+    return 0;
+}
