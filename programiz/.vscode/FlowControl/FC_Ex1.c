c0d886083a60f612b3da947de89819d90f2b4b75
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-13 14:13:11 +0530

    FlowControl-Examples

---
+++ programiz/.vscode/FlowControl/FC_Ex1.c
@@ -0,0 +1,12 @@
+// Program to display a number if it is negative
+#include<stdio.h>
+int main()
+{
+   int a;
+   printf("Enter a number: ");
+   scanf("%d",&a);
+   if(a<0){
+       printf("The number is: %d\n",a);
+   } 
+   printf("The number is easy");
+}
