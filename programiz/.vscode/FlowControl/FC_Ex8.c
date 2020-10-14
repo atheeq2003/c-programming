eb7e9765b7c8df332e967e9b16a14b6bce8a44e0
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-14 17:27:05 +0530

    FlowControl-Examples

---
+++ programiz/.vscode/FlowControl/FC_Ex8.c
@@ -0,0 +1,13 @@
+// Print numbers from 1 to 5
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    printf("Numbers from 1 to 5: ");
+    while (i<=5)
+    {
+        printf("%d ",i);
+        ++i;
+    }
+    return 0;  
+}
