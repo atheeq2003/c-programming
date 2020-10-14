7297e6a3c8b99fa589e7b50d5f9774c3f3ef9855
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-14 13:37:39 +0530

    FlowControl-Examples

---
+++ programiz/.vscode/FlowControl/FC_Ex5.c
@@ -0,0 +1,11 @@
+// Print numbers from 1 to 10 using for loop
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("The numbers are: ");
+    for(i=1;i<11;++i){
+        printf("%d ",i);
+    }
+    return 0;
+}
