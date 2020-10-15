b79c902a6a1307c5af99fa9a3517bc85b4e1eff7
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-15 17:35:45 +0530

    FlowControl-Examples

---
+++ programiz/.vscode/FlowControl/FC_Ex10.c
@@ -0,0 +1,15 @@
+// Program to calculate the sum of numbers (10 numbers max)
+// If the user enters a negative number, the loop terminates
+#include<stdio.h>
+int main()
+{
+    int i,num,sum=0;
+    for(i=1;i<6;++i){
+        if(num>0){
+            printf("Enter an number: ");
+            scanf("%d",&num);
+            sum = sum + num;
+        }   
+    }
+    printf("The sum is: %d",sum);
+}
