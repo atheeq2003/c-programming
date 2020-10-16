5a2977746b2414881bd48b0fbde884f1f2224f8c
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-16 17:52:49 +0530

    FlowControl-Examples

---
+++ programiz/.vscode/FlowControl/FC_Ex13.c
@@ -0,0 +1,22 @@
+// Program to create a simple calculator
+#include<stdio.h>
+int main()
+{
+    char operator;
+    double num1,num2;
+    printf("Enter the 1st Number: ");
+    scanf("%d",&num1);
+    printf("Enter the 2nd Number: ");
+    scanf("%d",&num2);
+    printf("Enter an operator: ");
+    scanf("%c",&operator);
+switch(operator){
+    case '+';
+    printf("%lf + %lf = %lf",num1,num2,num1+num2);
+    break;
+
+    case '-';
+    printf("%lf - %lf = %lf",num1,num2,num1+num2);
+    break;
+}
+)
