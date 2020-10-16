461fe9e70745d3c0af9305b4d4a29ec721b696a9
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-16 18:51:54 +0530

    FlowControl-Examples

--- programiz/.vscode/FlowControl/FC_Ex13.c
+++ programiz/.vscode/FlowControl/FC_Ex13.c
@@ -4,35 +4,31 @@ int main()
 {
     char operator;
     double num1,num2;
-    printf("Enter the 1st Number: ");
-    scanf("%d",&num1);
-    printf("Enter the 2nd Number: ");
-    scanf("%d",&num2);
     printf("Enter an operator: ");
     scanf("%c",&operator);
+    printf("Enter the 1st Number: ");
+    scanf("%lf",&num1);
+    printf("Enter the 2nd Number: ");
+    scanf("%lf",&num2);
  switch(operator){
-    case '+';
-    printf("%lf + %lf = %lf",num1,num2,num1+num2);
+    case '+':
+    printf("%.2lf + %.2lf = %.2lf",num1,num2,num1+num2);
     break;
 
-    case '-';
-    printf("%lf - %lf = %lf",num1,num2,num1-num2);
+    case '-':
+    printf("%.2lf - %.2lf = %.2lf",num1,num2,num1-num2);
     break;
 
-    case '*';
-    printf("%lf * %lf = %lf",num1,num2,num1*num2);
+    case '*':
+    printf("%.2lf * %.2lf = %.2lf",num1,num2,num1*num2);
     break;
 
-    case '/';
-    printf("%lf / %lf = %lf",num1,num2,num1/num2);
-    break;
-
-    case '%';
-    printf("%lf % %lf = %lf",num1,num2,num1%num2);
+    case '/':
+    printf("%.2lf / %.2lf = %.2lf",num1,num2,num1/num2);
     break;
 
     default:
     printf("Error! operator is not correct");
 }
 return 0;
-)
+}
