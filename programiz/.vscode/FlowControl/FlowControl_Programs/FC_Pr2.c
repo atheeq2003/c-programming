36f930188a2c81f77bc847dd3811f1b3d0aa7d99
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-17 17:04:35 +0530

    FlowControl-Programs

--- programiz/.vscode/FlowControl/FlowControl_Programs/FC_Pr2.c
+++ programiz/.vscode/FlowControl/FlowControl_Programs/FC_Pr2.c
@@ -2,11 +2,12 @@
 #include<stdio.h>
 int main()
 {
-    char c,vowel;
+    char c,vowel,VOWEL;
     printf("Enter a alphabet: ");
     scanf("%c",&c);
+    VOWEL = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
     vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-    if(c || vowel){
+    if(VOWEL || vowel){
         printf("The character is a vowel: %c",c);
     }else{
         printf("The character is a consonant: %c",c);
