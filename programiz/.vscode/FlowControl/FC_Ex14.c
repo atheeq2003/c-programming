3b0d9f0b345cb2959cef797733d5e5486fa20195
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-17 13:56:09 +0530

    FlowControl-Examples

---
+++ programiz/.vscode/FlowControl/FC_Ex14.c
@@ -0,0 +1,22 @@
+// Program to calculate the sum and average of positive numbers
+// If the user enters a negative number, the sum and average are displayed.
+#include<stdio.h>
+int main()
+{
+    const int maxInput = 100;
+    int i;
+    double number,average,sum=0.0;
+    for (i=1;i <= maxInput;++i){
+        printf("Enter a number: ");
+        scanf("%lf",&number);
+        if(number<0.0){
+            goto jump;
+        }
+        sum = sum + number;
+    }
+        jump:
+           average = sum / (i - 1);
+           printf("Sum = %.2f\n",sum);
+           printf("Average = %.2f", average);
+           return 0;
+}
