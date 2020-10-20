3c0c336bfb03f79d39849d414de0057f6468ab34
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-20 17:04:00 +0530

    FlowControl-Programs

--- programiz/.vscode/FlowControl/FlowControl_Programs/FC_Pr6.c
+++ programiz/.vscode/FlowControl/FlowControl_Programs/FC_Pr6.c
@@ -11,8 +11,8 @@ int main()
     if(result1 == 0){
         printf("It is a Leap Year.");
     }else if(result1 == result2){
-        printf("It is a Leap Year.");                                  
-    }else if(result3 == result2 == result3 == 1){
+        printf("It is not a Leap Year.");                                  
+    }else if(result3 == result2 == 0){
         printf("It is a Leap Year.");
     }else{
         printf("It is not a Leap Year.");
