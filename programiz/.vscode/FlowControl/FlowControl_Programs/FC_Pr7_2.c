230bbb6e3c15aabab216f1cdd0a78514df610f71
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-21 17:49:45 +0530

    FlowControl-Programs

---
+++ programiz/.vscode/FlowControl/FlowControl_Programs/FC_Pr7_2.c
@@ -0,0 +1,18 @@
+//Check Positive or Negative Using Nested if...else
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    if(num<=0){
+        if(num == 0){
+            printf("You've enterd 0.");
+        }else{
+            printf("It is an negative number.");
+        }
+    }else{
+        printf("It is an positive number.");
+    }
+    return 0;
+}
