11040540da061c83a186610bc977910ce7f7179f
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-22 17:43:07 +0530

    FlowControl-Programs

--- programiz/.vscode/FlowControl/FlowControl_Programs/FC_Pr11_2.c
+++ programiz/.vscode/FlowControl/FlowControl_Programs/FC_Pr11_2.c
@@ -1,11 +1,13 @@
-//C Program to Generate Multiplication Table
+//C Program to Generate Multiplication Table upto required range
 #include<stdio.h>
 int main()
 {
-    int i,num;
+    int i,num,range;
     printf("Which Multiplication Table Do You Want?\n");
     scanf("%d",&num);
-    for(i=1;i<11;++i){
+    printf("Enter the range: ");
+    scanf("%d",&range);
+    for(i=1;i<=range;++i){
         printf("%d * %d = %d\n",num,i,num*i);
     }
     return 0;
