d8b8fe2ccc9ccbf31719ac1613fa19a77a7ea1e8
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-27 18:47:13 +0530

    FlowControl-Programs

--- programiz/.vscode/FlowControl/FlowControl_Programs/FC_Pr13_3.c
+++ programiz/.vscode/FlowControl/FlowControl_Programs/FC_Pr13_3.c
@@ -10,14 +10,18 @@ int main()
     scanf("%d",&n1);
     printf("Enter the second integer: ");
     scanf("%d",&n2);
-    abs(n1);
-    abs(n2);
-    while(n1 != n2){
+    n1 = ( n1 > 0) ? n1 : -n1;
+    n2 = ( n2 > 0) ? n2 : -n2;
+    while(n1!=n2)
+    {
         if(n1 > n2){
-            n1 = n1 - n2;
-        }else{
-            n2 = n2 -n1;
+            n1 -= n2;
+        }
+        else{
+            n2 -= n1;
         }
     }
+    printf("GCD = %d",n1);
+
     return 0;
 }
