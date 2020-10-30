52fda29d4a3d55d50618e2d2fb7f34bd04d15049
atheeq2003 <atheequrrahaman8@gmail.com>
2020-10-30 14:38:46 +0530

    FlowControl-Programs

---
+++ programiz/.vscode/FlowControl/FlowControl_Programs/FC_Pr18.c
@@ -0,0 +1,19 @@
+//Program to Check Palindrome
+#include<stdio.h>
+int main()
+{
+    int original,reversed=0,remainder,n;
+    printf("Enter a integer: ");
+    scanf("%d",&n);
+    original = n;
+    while(n != 0){
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n = n / 10;
+    }if(original == reversed){
+        printf("The number is Palindrome.");
+    }else{
+        printf("The number is not Palindrome.");
+    }
+    return 0;
+}
